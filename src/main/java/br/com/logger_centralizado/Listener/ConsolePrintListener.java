5edd16ffcce18bc8f5053d2fe8bbe2bf350f015b
Eduardo <efmvigil@gmail.com>
2025-06-12 21:37:47 -0300

    implementada a interface de EventListener

---
+++ src/main/java/br/com/logger_centralizado/Listener/ConsolePrintListener.java
@@ -0,0 +1,9 @@
+package br.com.logger_centralizado.Listener;
+
+import br.com.logger_centralizado.Log;
+
+public class ConsolePrintListener implements EventListener{
+    private void update(Log log){
+
+    }
+}
